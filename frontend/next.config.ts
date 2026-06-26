9facc2ad0047828dcdea9aaf1bc4c0dd0562082f
Jonggun <tartaryoungx@gmail.com>
2026-06-27 00:58:41 +0700

    get it back

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
